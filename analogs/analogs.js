0573b00671769b216dffc729d9a1047355b40d24
Eliot Briefer <briefereliot@gmail.com>
2026-01-12 20:32:26 -0800

    Created rough game framework and styling

--- analogs/analogs.js
+++ analogs/analogs.js
@@ -1,1 +1,74 @@
 //analogs specific game logic
+import { GameCard } from '../game_components.js';
+
+class Analogs {
+    constructor(parent, relationship, bonus='false') {
+        this.parent = parent;
+        this.itemElements = [];
+
+        //Create DOM elements
+        this.card = new GameCard(parent, true, bonus);
+        this.card.hideTopText();
+        this.card.hideBottomText();
+        this.card.setTopText(relationship);
+    }
+
+    addPair(wordOne, wordTwo) {
+        let item1 = new Item(wordOne);
+        let item2 = new Item(wordTwo);
+        let pairDiv = document.createElement('div');
+        let seperator = document.createElement('p');
+        seperator.innerText = ':';
+        pairDiv.classList.add('pair');
+        pairDiv.appendChild(item1.element);
+        pairDiv.appendChild(seperator);
+        pairDiv.appendChild(item2.element);
+        this.card.gameElement.appendChild(pairDiv);
+    }
+}
+
+class Item {
+    constructor(text) {
+        this.text = text;
+        this.element = document.createElement('button');
+        this.element.classList.add('item');
+        this.p = document.createElement('p');
+        this.element.appendChild(this.p);
+        this.selected = false;
+        this.setText(text);
+        this.#initEvents();
+    }
+
+    setText(text) {
+        this.text = text;
+        this.p.innerText = text;
+    }
+
+    getText(text) {
+        return this.text;
+    }
+
+    #initEvents() {
+        this.element.addEventListener('click', () => {
+            this.select();
+        })
+    }
+
+    select() {
+        this.selected = true;
+        this.element.classList.add('selected');
+    }
+
+    deselect() {
+        this.selected = false;
+        this.element.classList.remove('selected');
+    }
+}
+
+
+
+
+const carousel = document.getElementById("carousel");
+
+const testGame = new Analogs(carousel, 'TESTING', true);
+testGame.addPair('Hello', 'But what about word wrap?');
--- game_components.js
+++ game_components.js
@@ -1,2 +1,95 @@
 //Contains classes for common game elements
-//Import into <game_name>.js to use
+//Import into <game_name>.js to use in game logic
+
+//------------CLASSES------------
+//Game card creates the main div for a game and common static game elemnts
+export class GameCard {
+    constructor(parent, hintButton=false, bonus=false) {
+        this.parent = parent;
+        this.won = false;
+        this.hinting = false;
+
+        //create DOM elements
+        this.listItem = document.createElement('li');
+        this.element = document.createElement('div');
+        this.element.classList.add('card');
+        if(bonus) {
+            this.element.classList.add('bonus');
+        }
+        this.topTextElement = document.createElement('p');
+        this.bottomTextElement = document.createElement('p');
+        this.gameElement = document.createElement('div');
+        this.gameElement.classList.add('game')
+
+        //create hint button
+        this.hintButton = document.createElement('button');
+        this.hintButton.textContent = "THROW ME A BONE";
+        this.hideHintButton();
+        
+        //Build DOM tree
+        this.element.appendChild(this.topTextElement);
+        this.element.appendChild(this.gameElement);
+        this.element.appendChild(this.bottomTextElement);
+        if(hintButton) {
+            this.element.appendChild(this.hintButton);
+        }
+        this.listItem.appendChild(this.element);
+        this.parent.appendChild(this.listItem);
+
+        //enable hint button after 45 seconds
+        setTimeout(() => {
+            if(this.won) return;
+            this.showHintButton();
+        }, 30000);
+    }
+
+    addHintFunction(hintFunction) {
+        this.hintButton.addEventListener('click', () => {
+            hintFunction();
+        })
+    }
+
+    //Game logic should call when the win conditions are met
+    win() {
+        this.won = true
+        this.hintButton.disable = true;
+    }
+
+    hideTopText() {
+        this.topTextElement.style.visibility = 'hidden';
+    }
+
+    showTopText() {
+        this.topTextElement.style.visibility = 'visible';
+    }
+
+    hideBottomText() {
+        this.bottomTextElement.style.visibility = 'hidden';
+    }
+
+    showBottomText() {
+        this.bottomTextElement.style.visibility = 'visible';
+    }
+
+    setTopText(text) {
+        this.topTextElement.innerText = text;
+    }
+
+    setBottomText(text) {
+        this.bottomTextElement.innerText = text;
+    }
+
+    hideHintButton() {
+        this.hintButton.disabled = true;
+    }
+
+    showHintButton() {
+        this.hintButton.disabled = false;
+    }
+}
+
+
+//----------FUNCTIONS------------
+export function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+}
