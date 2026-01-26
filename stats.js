def2c25677caa4443d452b655ab016637fd9bdbf
Eliot Briefer <briefereliot@gmail.com>
2026-01-25 21:02:33 -0800

    Updated dates on landing page

--- stats.js
+++ stats.js
@@ -1,7 +1,7 @@
 import { LabelCard } from "./game_components.js";
 
 let wordWebsCard = document.getElementById('word-webs');
-let wordWebsDate = new LabelCard(wordWebsCard, "Jan 25th");
+let wordWebsDate = new LabelCard(wordWebsCard, "Jan 26th");
 
 let analogsCard = document.getElementById('analogs');
-let analogsDate = new LabelCard(analogsCard, "Jan 25th");
+let analogsDate = new LabelCard(analogsCard, "Jan 26th");
